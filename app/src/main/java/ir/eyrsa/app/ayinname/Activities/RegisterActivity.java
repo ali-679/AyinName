86842051aff24fa566df72009bb0a303a37f71de
Ali <fzali.679@gmail.com>
2022-10-27 04:51:21 +0330

    init

---
+++ app/src/main/java/ir/eyrsa/app/ayinname/Activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package ir.eyrsa.app.ayinname.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.eyrsa.app.ayinname.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
