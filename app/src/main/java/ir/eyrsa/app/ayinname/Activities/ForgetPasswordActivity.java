44f808aecb1db6fe721e0f1906f0c650df48586a
Ali <fzali.679@gmail.com>
2022-10-27 06:01:03 +0330

    init

---
+++ app/src/main/java/ir/eyrsa/app/ayinname/Activities/ForgetPasswordActivity.java
@@ -0,0 +1,16 @@
+package ir.eyrsa.app.ayinname.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.eyrsa.app.ayinname.R;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
